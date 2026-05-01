afe14c29bedc89909c7f1ae46cd53af94faca5e8
Ayan Koley <koleyayan07@gmail.com>
2026-05-01 11:50:09 +0530

    Initialized typescript

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name}, welcome to consistent learning`;
+}
+//# sourceMappingURL=index.js.map
